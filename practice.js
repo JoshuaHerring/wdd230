5fe4f85487e3e8848890b2018d9dff345b746d69
Joshua Herring <jdherring2222@gmail.com>
2022-09-14 11:13:49 -0600

    practiced java

---
+++ practice.js
@@ -0,0 +1,17 @@
+// Sequence
+
+let date = new Date();
+
+console.log(date);
+
+let year = date.getFullYear();
+
+console.log(year);
+
+document.querySelector('h1').innerHTML = year;
+
+let current_date = document.lastModified;
+
+console.log(current_date);
+
+document.querySelector('div').textContent = current_date;
