ff16b5728049364239957780ff7217c542fc9f87
Joshua Herring <jdherring2222@gmail.com>
2022-10-05 11:44:24 -0600

    fixed javascript

--- lesson3/script/main.js
+++ lesson3/script/main.js
@@ -1,10 +1,9 @@
 button = document.getElementById("button");
 dropDown = document.getElementById("dropDown");
 width = screen.width;
-if(width <= 580)
-{
+
 dropDown.classList.toggle("vanish");
-}
+
 function display()
 {
     dropDown.classList.toggle("vanish");
