21da7b9588ae375519cbd5253314da5fab254665
Joshua Herring <jdherring2222@gmail.com>
2022-10-12 08:46:37 -0600

    added updating dates

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -10,3 +10,13 @@ function display()
 }
 
 button.addEventListener("click", display);
+
+var date = new Date();
+console.log(date);
+var year = date.getFullYear();
+console.log(year);
+
+document.querySelector("#date").innerHTML = year;
+
+let current_date = document.lastModified;
+document.querySelector("#last_updated").innerHTML = current_date;
