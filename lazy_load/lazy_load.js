2a47247dc2affd3d6db2e95388523571c95f5027
Joshua Herring <jdherring2222@gmail.com>
2022-10-27 10:14:52 -0600

    working but backwards

--- lazy_load/lazy_load.js
+++ lazy_load/lazy_load.js
@@ -1,32 +1,30 @@
 const image = document.querySelectorAll('img');
-const options = {
-    threshold: 1,
-    rootMargin: "0px 0px 100px 100px"
-}
-const io = new IntersectionObserver(
-    (entries, io) => {
-        entries.forEach(entry =>{
-            if (!entry.isIntersecting){
-                return;
-            } else {
-                preloadImage(entry.target);
-                io.unobserve(entry.target);
-            }
-
-        })
-        console.log(io);
-    },
-    {
-        threshold: [.5]
-    }
-)
-
-image.forEach(image => {
-    io.observe(image);
-})
-
-
 const pimages = document.querySelectorAll('[data-src]');
+const options = {
+    threshold: .5,
+    rootMargin: "0px 0px 100px 0px"
+}
+// const io = new IntersectionObserver(
+//     (entries, io) => {
+//         entries.forEach(entry =>{
+//             if (!entry.isIntersecting){
+//                 return;
+//             } else {
+//                 preloadImage(entry.target);
+//                 io.unobserve(entry.target);
+//             }
+
+//         })
+//         console.log(io);
+//     },
+//     {
+//         threshold: [.5]
+//     }
+// )
+
+// image.forEach(image => {
+//     io.observe(image);
+// })
 
 function preloadImage(img)
 {
@@ -36,3 +34,23 @@ function preloadImage(img)
     }
     img.src = source;
 }
+
+
+const io = new IntersectionObserver(
+        (entries, io) => {
+            entries.forEach(entry =>{
+                if (!entry.isIntersecting){
+                    return;
+                } else {
+                    preloadImage(entry.target);
+                    io.unobserve(entry.target);
+                }
+    
+            })
+            // console.log(io);
+        },
+        options);
+
+        pimages.forEach(image => {
+            io.observe(image);
+        })
