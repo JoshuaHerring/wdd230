6db98bb4b220b5e22e35a0cf9e003f6abf41df70
Joshua Herring <jdherring2222@gmail.com>
2022-11-09 12:59:42 -0700

    asdded on to prophets

--- api_practice/prophets.js
+++ api_practice/prophets.js
@@ -20,8 +20,22 @@ function displayProphets(item)
 {
     let card = document.createElement('section')
     let h2 = document.createElement('h2')
+    let birthday = document.createElement('p')
+    let birthplace = document.createElement('p')
+    let img = document.createElement('img')
+
+
     h2.innerHTML = item.name + " " + item.lastname
+    birthday.innerHTML = item.birthdate
+    birthplace.innerHTML = item.birthplace
+    img.setAttribute('src', item.imageurl)
+    img.setAttribute('alt', item.name + ' ' + item.lastname)
+
     card.appendChild(h2)
+    card.appendChild(birthday)
+    card.appendChild(birthplace)
+    card.appendChild(img)
+
     document.querySelector('.cards').appendChild(card)
 }
 
