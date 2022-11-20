e4ad4f5869ad18a2d380d80ec2ef05a1c870522e
Joshua Herring <jdherring2222@gmail.com>
2022-11-19 19:26:43 -0700

    finished the weather section

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -58,7 +58,7 @@ function calculate_difference()
     console.log(difference);
     let days_difference = Math.floor(difference/1000/60/60/24);
 
-    document.getElementById("diff").innerHTML = days_difference;
+    // document.getElementById("diff").innerHTML = days_difference;
 
     localStorage.setItem("last_visit", Date.now());
 }
@@ -66,4 +66,45 @@ function calculate_difference()
 
 // Join page date handling
 const currentDate = new Date();
-document.querySelector("#formDate").value = currentDate;
+// document.querySelector("#formDate").value = currentDate;
+
+
+// Weather handaling happens here
+
+const temp = document.getElementById("temp")
+const cloud_coverage = document.getElementById("cloud_coverage")
+const wind_speed = document.getElementById("wind_speed")
+const wind_chill = document.getElementById("wind_chill")
+const icon = document.getElementById("weather_icon")
+// [40.5308, -112.2983]     coords to my town
+const api_url = "https://api.openweathermap.org/data/2.5/weather?lat=40.5308&lon=-112.2983&appid=92ce6425ce6e4ca46b7b78c529993dfc"
+
+async function getWeather(api_url)
+{
+    const response = await fetch(api_url)
+    console.log(response)
+    if(response.ok)
+    {
+        const jsonObject = await response.json()
+        console.log(jsonObject)
+        displayWeather(jsonObject)
+    }
+}
+
+function displayWeather(jsonObject)
+{
+    temp.innerHTML = "   " + kelvinToFarenheight(jsonObject["main"]["temp"])
+    cloud_coverage.innerHTML = jsonObject["weather"][0]["description"]
+    wind_speed.innerHTML = jsonObject["wind"]["speed"]
+    icon.setAttribute("src", "https://openweathermap.org/img/w/" + jsonObject["weather"][0]["icon"] + ".png")
+    // console.log( "&#" + jsonObject["weather"][0]["icon"] + ";")
+}
+
+function kelvinToFarenheight(kelvin)
+{
+    let farenheight = (kelvin - 273.15) * 9/5 + 32
+    farenheight = farenheight.toFixed(2)
+    return farenheight
+
+}
+getWeather(api_url)
