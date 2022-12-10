12bd868183aaf2b2130d42159164f35098c2287f
Joshua Herring <jdherring2222@gmail.com>
2022-12-10 13:04:34 -0700

    made a lot of progress

--- personal_project/js/index.js
+++ personal_project/js/index.js
@@ -1,8 +1,12 @@
-/// Handles the hero image picture changing
+/// Handles the hero image picture changing for the home page
 
 let screen_width = window.innerWidth;
 let hero_image = document.querySelector(".hero_img")
 if (screen_width >= 800)
 {
-    hero_image.src = "./images/forest_wide.webp"
+    hero_image.src = "./images/forest_wide.webp";
+}
+else
+{
+    hero_image.src = "./images/cress_creek_sunset.webp";
 }
